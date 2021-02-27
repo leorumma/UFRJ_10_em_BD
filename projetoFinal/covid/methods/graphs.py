90541b1718a3f7239e47d371ab955500972741a8
NasreddinHodja <tbizetde@gmail.com>
2021-02-27 18:00:52 -0300

    general function for charts

--- projetoFinal/covid/methods/graphs.py
+++ projetoFinal/covid/methods/graphs.py
@@ -1,21 +1,14 @@
 import pandas as pd
 
-def get_bar_chart(df: pd.DataFrame, x_column, y_column, width=80):
+def get_chart(df: pd.DataFrame, x_column, y_column, width=80):
 
     bg_color = "rgba(158,202,225,0.8)"
 
     if not isinstance(df, pd.DataFrame):
         raise Exception("Not a pandas dataFrame")
 
-    return {
-            "labels": df[x_column].tolist(),
-            "datasets": [{
-                "label": "",
-                "data": df[y_column].tolist(),
-                "backgroundColor": bg_color,
-                "barThickness": width,
-            }]
-        }
+    return dict(zip(df[x_column].tolist(), df[y_column].tolist()))
+
 
 def get_card(value: str, desc: str, icon: int) -> dict:
     # ajust
@@ -24,8 +17,4 @@ def get_card(value: str, desc: str, icon: int) -> dict:
     if len(desc) > max_desc_len:
         raise Exception(f'Description text too big. Keep under {max_desc_len} chars')
 
-    return {
-        "value": value,
-        "desc":  desc,
-        "icon":  icon
-    }
+    return {"value": value, "desc":  desc}
--- projetoFinal/covid/methods/test.py
+++ projetoFinal/covid/methods/test.py
@@ -7,11 +7,18 @@ import covid.methods.graphs as graphs
 
 def test_query():
     query = """
-    select table_type, count(table_name)
+    select table_type, (count(table_name)::decimal / max(t.total)) as percent
     from information_schema.tables
+    cross join (
+        select total from (
+            select 'anchor' as anchor, count(table_name) as total
+            from information_schema.tables
+            group by anchor
+        ) as t1
+    ) as t
     group by table_type
     """
 
     df = pd.read_sql(query, con=con.get())
 
-    return graphs.get_bar_chart(df, "table_type", "count")
+    return graphs.get_chart(df, "table_type", "percent")
