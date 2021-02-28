86c4655763668e5328b39fe0e6619a982d8dff20
angelo-daumas <42705006+angelo-daumas@users.noreply.github.com>
2021-02-28 16:56:24 -0300

    fix: vue production environment

--- projetoFinal/webapp/config/prod.env.js
+++ projetoFinal/webapp/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
   APP_NAME: '"10 em BD"',
-  API_PATH: '"http://34.123.117.26:8000"'
+  API_PATH: '"http://34.123.117.26:8000"',
 }
