c8987fe7595fea8c4965a96993ac29a981ca2e00
NasreddinHodja <tbizetde@gmail.com>
2021-02-28 16:39:27 -0300

    group bar chart sketch

--- projetoFinal/covid/methods/graphs.py
+++ projetoFinal/covid/methods/graphs.py
@@ -18,3 +18,29 @@ def get_card(value: str, desc: str, icon: int) -> dict:
         raise Exception(f'Description text too big. Keep under {max_desc_len} chars')
 
     return {"value": value, "desc":  desc}
+
+def get_grouped_bar_chart(df: pd.DataFrame, x_column, y_column_trace1, y_column_trace2,
+                           name_trace1, name_trace2, width=80):
+
+    if not isinstance(df, pd.DataFrame):
+        raise Exception("Not a pandas dataFrame")
+
+    trace1 = {
+        "label": name_trace1,
+        "backgroundColor": "rgba(90, 230, 213, 0.8)",
+        "barThickness": width,
+        "data": df[y_column_trace1].tolist()
+    }
+
+    trace2 = {
+        "label": name_trace2,
+        "backgroundColor": "rgba(90, 230, 213, 0.8)",
+        "barThickness": width,
+        "data": df[y_column_trace2].tolist(),
+    }
+
+    return {
+        #"labels": df[x_column].tolist(),
+        "labels": anonymize(df[x_column].tolist()),
+        "datasets": [trace1, trace2]
+    }
