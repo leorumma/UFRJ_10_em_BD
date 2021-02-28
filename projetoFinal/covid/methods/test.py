a32a4ff52d10fc1f20b9edc599a829ff9379c927
NasreddinHodja <tbizetde@gmail.com>
2021-02-28 17:59:33 -0300

    teste grouped bar chart

--- projetoFinal/covid/methods/test.py
+++ projetoFinal/covid/methods/test.py
@@ -6,19 +6,27 @@ import covid.methods.connection as con
 import covid.methods.graphs as graphs
 
 def test_query(**kwargs):
-    query = """
-    select table_type, (count(table_name)::decimal / max(t.total)) as percent
-    from information_schema.tables
-    cross join (
-        select total from (
-            select 'anchor' as anchor, count(table_name) as total
-            from information_schema.tables
-            group by anchor
-        ) as t1
-    ) as t
-    group by table_type;
-    """
+    # query = """
+    # select table_type, (count(table_name)::decimal / max(t.total)) as percent
+    # from information_schema.tables
+    # cross join (
+    #     select total from (
+    #         select 'anchor' as anchor, count(table_name) as total
+    #         from information_schema.tables
+    #         group by anchor
+    #     ) as t1
+    # ) as t
+    # group by table_type;
+    # """
 
-    df = pd.read_sql(query, con=con.get())
+    # df = pd.read_sql(query, con=con.get())
 
-    return graphs.get_chart(df, "table_type", "percent")
+    # exemplo grouped bar chart
+
+    df = pd.DataFrame([['giraffes', 20, 12],
+                       ['orangutang', 14, 18],
+                       ['monkeys', 23, 29]],
+                      columns=['labels', 'trace1', 'trace2'])
+
+    return graphs.get_grouped_bar_chart(df, 'labels', 'trace1', 'trace2',
+                                        'SF Zoo', 'LA Zoo')
