b391b878ec698c5d93ed7c1b154fc015e70774a3
angelo-daumas <42705006+angelo-daumas@users.noreply.github.com>
2021-02-28 15:46:00 -0300

    feat: título e tipo dos gráficos no JSON

--- projetoFinal/covid/methods/page.py
+++ projetoFinal/covid/methods/page.py
@@ -4,20 +4,33 @@ from covid.methods.test import test_query
 from . import age_groups
 from . import outcomes
 
+
+from collections import defaultdict
+
+#TODO: melhorar isso aqui, fazer uma classe em vez de vários dicts
 methods = {
 	"/teste/" : test_query,
 	"/age_groups/": age_groups.do_query,
 	"/outcomes/": outcomes.do_query,
 }
 
+titles = defaultdict(lambda : "Test Title", {
+	"/age_groups/": "Faixa Etária dos Pacientes",
+	"/outcomes/": "Sumário dos Desfechos",
+})
+
+types = defaultdict(lambda : "pie", {
+
+})
+
 def __page(path, queryargs={}):
 
 	output = {
 		# teste
-		"title": "teste title",
+		"title": titles[path],
 		"subtitle": "teste subtitle",
 		"description": "teste description",
-		"type": "test type",
+		"type": types[path],
 		"content": methods[path](**queryargs),
     }
 
